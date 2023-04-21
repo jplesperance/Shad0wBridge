94b8fd2ae6515854a5f98094423525772e493a1f
Jesse Lesperance <jesse@lesperance.io>
2023-04-21 15:47:56 -0700

    adding functions for sockets

--- client.py
+++ client.py
@@ -3,10 +3,11 @@ import socket
 target_ip = '127.0.0.1'
 target_port = 2222
 
+def session_handler():
+    print(f'[+] Connecting to {target_ip}.')
+    sock.connect((target_ip, target_port))
+    print(f'[+] Connected to {target_ip}.')
+    sock.close()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print(f'[+] Connecting to {target_ip}.')
-sock.connect((target_ip, target_port))
-print(f'[+] Connected to {target_ip}.')
-
-
-sock.close()
+session_handler()
