ad85a7be17bd0d2ce5969e1743153c397825ce78
Jesse Lesperance <jesse@lesperance.io>
2023-04-21 14:34:32 -0700

    wrapped socket functions in a listener function

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import socket
 
+def listener_handler():
+    sock.bind((host_ip, host_port))
+    print('[+] Awaiting connection from client...')
+    sock.listen()
+    remote_target, remote_ip = sock.accept()
+    print(f'[+] Connection received from {remote_ip[0]}')
+    remote_target.close()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host_ip = '127.0.0.1'
 host_port = 2222
-
-sock.bind((host_ip, host_port))
-print('[+] Awaiting connection from client...')
-sock.listen()
-remote_target, remote_ip = sock.accept()
-print(f'[+] Connection received from {remote_ip[0]}')
+listener_handler()
 
 
-
-remote_target.close()
