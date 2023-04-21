4c7164de85f31751679d6e989a67361248b74fb3
Jesse Lesperance <jesse@lesperance.io>
2023-04-21 16:12:12 -0700

    Added 2-way communicaiton and graceful exit

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 target_ip = '127.0.0.1'
 target_port = 2222
 
@@ -7,7 +9,28 @@ def session_handler():
     print(f'[+] Connecting to {target_ip}.')
     sock.connect((target_ip, target_port))
     print(f'[+] Connected to {target_ip}.')
-    sock.close()
+    while True:
+        try:
+            print('[+] Awaiting response...')
+            message = sock.recv(1024).decode()
+            if message == 'exit':
+                print('[-] The server has terminated the session.')
+                sock.close()
+                break
+            print(message)
+            response = input('Message to send#> ')
+            if response == 'exit':
+                sock.send(response.encode())
+                sock.close()
+                break
+            sock.send(response.encode())
+        except KeyboardInterrupt:
+            print('[+] Keyboard interrupt issued.')
+            sock.close()
+            break
+        except Exception:
+            sock.close()
+            break
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 session_handler()
--- server.py
+++ server.py
@@ -1,17 +1,40 @@
 import socket
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host_ip = '127.0.0.1'
+host_port = 2222
+
 def listener_handler():
     sock.bind((host_ip, host_port))
     print('[+] Awaiting connection from client...')
     sock.listen()
     remote_target, remote_ip = sock.accept()
     print(f'[+] Connection received from {remote_ip[0]}')
-    remote_target.close()
+    while True:
+        try:
+            message = input('Message to send#> ')
+            if message == 'exit':
+                remote_target.send(message.encode())
+                remote_target.close()
+                break
+            remote_target.send(message.encode())
+            response = remote_target.recv(1024).decode()
+            print(response)
+            if response == 'exit':
+                print('[-] The client has terminated the session.')
+                remote_target.close()
+                break
+            print(response)
+        except KeyboardInterrupt:
+            print('[+] Keyboard interrupt issued.')
+            remote_target.close()
+            break
+        except Exception:
+            remote_target.close()
+            break
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host_ip = '127.0.0.1'
-host_port = 2222
 listener_handler()
 
 
